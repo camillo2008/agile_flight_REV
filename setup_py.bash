7a008b12d799fcec6c8e0f5b48e378309c8498c2
Michael Bosello <michael.bosello@unibo.it>
2022-04-04 18:50:43 +0200

    Make the conda env name match the one indicated in environment.yaml

--- setup_py.bash
+++ setup_py.bash
@@ -52,7 +52,7 @@ source ~/anaconda3/etc/profile.d/conda.sh
 
 # 
 echo "Actiavte the environment"
-conda activate agileflight
+conda activate agileflight0
 
 echo "Compiling the agile flight environment and install the environment as python package"
 cd $project_path/flightmare/flightlib/build
