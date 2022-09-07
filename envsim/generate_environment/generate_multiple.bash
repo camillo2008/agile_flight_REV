750a50af11bed7b8a9c48ba8ee781a8cd85ddef2
Gyordan <jordancaminati@libero.it>
2022-09-07 22:17:59 +0200

    Modified obstacles generator

--- envsim/generate_environment/generate_multiple.bash
+++ envsim/generate_environment/generate_multiple.bash
@@ -9,13 +9,26 @@ else
   N=$1
 fi
 
+if [ -z $2 ]
+then
+  SINGLE_SPHERE_PARAM=1
+else
+  SINGLE_SPHERE_PARAM=$2
+fi
+
+if [ -z $3 ]
+then
+  DENSITY_MULTIPLIER=1
+else
+  DENSITY_MULTIPLIER=$3
+fi
 rm -rf environment*
 
 for i in $(eval echo {0..$N})
 do
   dirname="environment_""$i"
   mkdir -p $dirname
-  python obstacle_generator.py "$RANDOM"
+  python obstacle_generator.py "$RANDOM" $SINGLE_SPHERE_PARAM $DENSITY_MULTIPLIER
   mv csvtrajs $dirname
   mv dynamic_obstacles.yaml $dirname
   mv static_obstacles.csv $dirname
